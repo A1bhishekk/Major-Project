e3336cc405fc91d38710f29da0ac3b416e7dfe8a
Abhishek Kumar <sahniak56@gmail.com>
2023-01-01 16:27:12 +0530

    g

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider,theme } from '@chakra-ui/react';
+import './index.css'
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
